e11dbbbccfc4a432700f6fc3c1198138ef73b8ac
hernani <megahack9@outlook.com>
2020-03-07 23:09:31 -0300

    reescrevendo codigo usando funcao switch

--- src/choiceCalendar/Calendar.java
+++ src/choiceCalendar/Calendar.java
@@ -8,55 +8,48 @@ public class Calendar {
 	public static void main(String[] args) {
 		String aux = JOptionPane.showInputDialog(null, "Digite um numero de 1 a 12");
 		month = Integer.parseInt(aux);
-		if (month == 1) {
+		switch(month){
+		case  1 :
 			JOptionPane.showMessageDialog(null, "Janeiro");
-		} else {
-			if (month == 2) {
-				JOptionPane.showMessageDialog(null, "Fevereiro");
-			} else {
-				if (month == 3) {
-					JOptionPane.showMessageDialog(null, "Marco");
-				} else {
-					if (month == 4) {
-						JOptionPane.showMessageDialog(null, "Abril");
-					} else {
-						if (month == 5) {
-							JOptionPane.showMessageDialog(null, "Maio");
-						} else {
-							if (month == 6) {
-								JOptionPane.showMessageDialog(null, "Junho");
-							} else {
-								if (month == 7) {
-									JOptionPane.showMessageDialog(null, "Julho");
-						} else {
-							if (month == 8) {
-								JOptionPane.showMessageDialog(null, "Agosto");
-					} else {
-						if (month == 9) {											JOptionPane.showMessageDialog(null, "Setembroo");
-				} else {
-					if (month == 10) {
-							JOptionPane.showMessageDialog(null, "Outubro");
-			} else {
-				if (month == 11) {
-						JOptionPane.showMessageDialog(null, "Novembro");
-		} else {
-			if (month == 12) {
-					JOptionPane.showMessageDialog(null, "Dezembro");
-	}else {
-		JOptionPane.showMessageDialog(null, "Numero invalido");
+		break;
+		case 2:
+			JOptionPane.showMessageDialog(null, "Fevereiro");
+		break;
+		case 3:
+			JOptionPane.showMessageDialog(null, "Marco");
+		break;
+		case 4:
+			JOptionPane.showMessageDialog(null, "Abril");
+		break;
+		case 5:
+			JOptionPane.showMessageDialog(null, "Maio");
+	    break;
+		case 6:
+			JOptionPane.showMessageDialog(null, "Junho");
+		break;
+		case 7:
+			JOptionPane.showMessageDialog(null, "Julho");
+		break;
+		case 8:
+			JOptionPane.showMessageDialog(null, "Agosto");
+		break;
+		case 9:		
+			JOptionPane.showMessageDialog(null, "Setembroo");
+		break;
+		case 10:
+			JOptionPane.showMessageDialog(null, "Outubro");
+		break;
+		case 11:
+			JOptionPane.showMessageDialog(null, "Novembro");
+		break;
+		case 12:
+			JOptionPane.showMessageDialog(null, "Dezembro");
+	    break;
+		default:
+		JOptionPane.showMessageDialog(null, "Mes invalido");
+		break;
 	}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
+												
 	}
 
 }
